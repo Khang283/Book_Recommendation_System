c22409626eda9c899fb9b9fb8f16e2f35ec064ce
PhanNhaan <thanhnhan200177@gmail.com>
2023-11-14 13:12:15 +0700

    login

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -3,13 +3,15 @@ import random
 
 from flaskr import module
 from werkzeug.exceptions import abort
+from werkzeug.security import generate_password_hash, check_password_hash
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, session
 
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
+    app.secret_key = 'your_secret_key'
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
@@ -36,18 +38,109 @@ def create_app(test_config=None):
     @app.route('/home')
     @app.route('/')
     def home():
+
+        user = session.get('user')
         books =module.getListRandomBook()
-        return render_template('home.html', books =books)
+        return render_template('home.html', books =books, user=user)
     
     @app.route('/<int:book_id>', methods=['GET', 'POST'])
     def detail(book_id):
+        # books = module.getList('./data/metadata.json')
+        # ratings =module.getList('./data/ratings.json')
+        # module.test()
         book = module.getBookbyId(book_id)
-
+        if 'user' in session:
+            user = session.get('user')
+            rating_user = module.getRatingBookByUser(book_id, user['user_id'])
+            return render_template('detail.html', book=book, user=user, rating_user= rating_user)
+        
         # if request.method == 'POST':
-        #     rating = request.form.get('rating')
-        #     print(rating)
-            # comment = request.form.get('comment')
+        #     module.test()
+        #     rating_input = request.form['rating-input']
+        #     module.test()
+        #     rating = {
+        #         "item_id": book_id,
+        #         "user_id": session.get('user')['user_id'],
+        #         "rating": rating_input
+        #     }
+        #     module.test()
+
+        #     module.add_Rating(rating)
+        #     # print(rating)
+        #     return redirect(url_for('detail', book_id))
+            # return render_template('detail.html', book=book, user=user, rating_user= rating)
 
         return render_template('detail.html', book=book)
+    
+    @app.route('/submit_rating', methods=['POST'])
+    def submit_rating():
+        if request.method == 'POST':
+            rating_input = request.form['rating-input']
+            book_id = request.form['book_id']
+            rating = {
+                "item_id": int(book_id),
+                "user_id": session.get('user')['user_id'],
+                "rating": int(rating_input)
+            }
+
+            module.add_Rating(rating)
+            # print(rating)
+            return redirect(url_for('detail', book_id = book_id))
+    
+    @app.route('/register', methods=['GET', 'POST'])
+    def register():
+        
+        if request.method == 'POST':
+            username = request.form['username']
+            password = request.form['password']
+            hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
+
+            users = module.getList('./data/users.json')
+            user_id = module.get_max_user_id(users) +1
+
+            if username not in users:
+                user = {
+                    "user_id": user_id,
+                    "username": username,
+                    "password": hashed_password
+                }
+                if module.add_new_user(user, users):
+                    return redirect(url_for('login'))
+                else:
+                    error = 'Không thể đăng kí, xin vui lòng đăng kí lại.'
+                    return render_template('register.html', error=error)
+                
+            else:
+                # Tên người dùng đã tồn tại, hiển thị thông báo lỗi.
+                error = 'Tên này đã có người dùng. Xin hãy chọn tên người dùng khác.'
+                return render_template('register.html', error=error)
+
+        return render_template('register.html')
+    
+    @app.route('/login', methods=['GET', 'POST'])
+    def login():
+        
+        if request.method == 'POST':
+            username = request.form['username']
+            password = request.form['password']
+            users = module.getList('./data/users.json')
+            user = [i for i in users if username == i['username']]
+            if len(user) ==0:
+                error = 'Đăng nhập không hợp lệ. Làm ơn kiểm tra tên người dùng của bạn.'
+                return render_template('login.html', error=error)
+            else:
+                if check_password_hash(user[0]['password'], password):
+                    session['user'] = user[0] 
+                    return redirect(url_for('home'))
+                else:
+                    error = 'Đăng nhập không hợp lệ. Làm ơn kiểm tra mật khẩu của bạn.'
+                    return render_template('login.html', error=error)
+
+        return render_template('login.html')
+    
+    @app.route('/logout')
+    def logout():
+        session.pop('user', None)
+        return redirect(url_for('login'))
 
     return app
--- flaskr/module.py
+++ flaskr/module.py
@@ -2,24 +2,18 @@ import json
 import random
 from werkzeug.exceptions import abort
 
-def getListBooks():
+def getList(url):
     data=[]
-    with open('./data/metadata.json', 'r') as f:
+    with open(url, 'r') as f:
         for line in f:
                 data.append(json.loads(line))
+    print(url)
     return data
 
-def getListRatings():
-    data=[]
-    with open('./data/ratings.json', 'r') as f:
-        for line in f:
-                data.append(json.loads(line))
-    return data
+books = getList('./data/metadata.json')
+ratings =getList('./data/ratings.json')
 
-books = getListBooks()
-ratings =getListRatings()
-
-def getRatingavg(book_id):
+def getRatingavg(book_id, ratings):
     list_ratings = []
     for rating in ratings:
         if (book_id ==rating['item_id']):
@@ -30,18 +24,80 @@ def getRatingavg(book_id):
     average_rating = total_rating / len(list_ratings)
     return round(average_rating, 2)
 
+def getRatingBookByUser(book_id, user_id):
+    rating =[i for i in ratings if book_id == i['item_id'] and user_id == i['user_id']]
+    if len(rating)==0:
+        return 0
+    return rating[0]['rating']
+
+def add_Rating(rating):
+
+    ratings.append(rating)
+    print(rating['rating'])
+    if rating not in ratings:
+        try:
+            with open("./data/rating.json", 'w') as file:
+                for obj in ratings:
+                    json.dump(obj, file)
+                    file.write('\n')
+        except FileNotFoundError:
+            return False
+    else:
+        for entry in ratings:
+            if entry.get("item_id") == rating['item_id'] and entry.get("user_id") == rating['user_id']:
+                entry = rating
+                print(entry['rating'])
+                break
+        try:
+            with open("./data/ratings.json", 'w') as file:
+                for obj in ratings:
+                    json.dump(obj, file)
+                    file.write('\n')
+        except FileNotFoundError:
+            return False
+
+    return True
+
+def test():
+    print('test')
+# book
+
 def getBookbyId(book_id):
+    # books = getList('./data/metadata.json')
+    # ratings =getList('./data/ratings.json')
 
     book = [i for i in books if book_id == i['item_id']]
-    if book is None:
+    if len(book)==0:
         abort(404)
-    book[0]['ratingavg'] = getRatingavg(book_id)
+    book[0]['ratingavg'] = getRatingavg(book_id, ratings)
     return book[0]
 
 def getListRandomBook():
+    # books = getList('./data/metadata.json')
+    # ratings =getList('./data/ratings.json')
+
     listBooks = random.sample(books,10)
     for book in listBooks:
-         book['ratingavg'] = getRatingavg(book['item_id'])
+         book['ratingavg'] = getRatingavg(book['item_id'], ratings)
     return listBooks
 
+# user
 
+# Hàm này trả về giá trị lớn nhất của user_id trong dữ liệu
+def get_max_user_id(users):
+    if not users:
+        return 0
+    return max(item['user_id'] for item in users)
+
+# theem user
+def add_new_user(user, users):
+    users.append(user)
+    try:
+        with open("./data/users.json", 'w') as file:
+            for obj in users:
+                json.dump(obj, file)
+                file.write('\n')
+    except FileNotFoundError:
+        return False
+
+    return True
